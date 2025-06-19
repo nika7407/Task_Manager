64650f46c6e7876b367b0775a069f4a5b2ddc9a2
nika7407 <nikala7407@gmail.com>
2025-06-19 18:19:37 +0400

    1# first commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,10 @@ repositories {
 	mavenCentral()
 }
 
+application {
+	mainClass = "hexlet.code.AppApplication"
+}
+
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
